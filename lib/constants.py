409fe683842b2584d34c8f88a9f72ccf8e5f1ec5
Jake Mayhew <49874223+deletehead@users.noreply.github.com>
2023-09-08 12:50:38 -0400

    Create constants.py

---
+++ lib/constants.py
@@ -0,0 +1,13 @@
+### Constants File ###
+##
+## Modify the dict(s) as-needed.
+## Leave these default if you specify in cmd line.
+##
+
+api_info = {
+    "BHCE_DOMAIN"    : "bloodhound.apps.cpd.isd.upmc.edu",    # change to "cpp" for prod or "cpd" for dev
+    "BHCE_PORT"      : 443,
+    "BHCE_SCHEME"    : "https",
+    "BHCE_TOKEN_ID"  : "4bbe137a-dead-beef-d34d-2dc0ff33aabb",                     # Sample ID
+    "BHCE_TOKEN_KEY" : "1fLUv3Kbd9CkHe6Ea27bTGP1WF3wk45L63dJFNaaNKfPNbXFa7e3Z2=="  # Sample key
+}
